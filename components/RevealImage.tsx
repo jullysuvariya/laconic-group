c4ef77ddf0feb1cea7cd989242bb4d2577859adb
Jully <jullysitapara@gmail.com>
2024-05-25 23:24:51 +0530

    animation change

--- components/RevealImage.tsx
+++ components/RevealImage.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { motion } from 'framer-motion';
+import React, { useEffect } from 'react'
+import { motion, useAnimation } from 'framer-motion';
 
-export const RevelImage = ({ src, className }: { src: string, className: string }) => {
+export const RevelImage = ({ src, className }: { src: string , className: string }) => {
 
     const variants = {
         hidden: { opacity: 0, scale: 0.5 },
@@ -12,12 +12,17 @@ export const RevelImage = ({ src, className }: { src: string, className: string 
         <motion.img
             initial="hidden"
             whileInView="visible"
-            viewport={{ once: true }}
             variants={variants}
-            transition=
-            {{
-                duration: 0.4,
+            transition={{
+                duration: 0.3,
                 delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                    type: "spring",
+                    damping: 3,
+                    stiffness: 100,
+                    restDelta: 0.001
+                }
             }}
             src={src}
             className={className}
