ca5f0b2bf1e37ee337f1ccbfaf9273bcaa4dcd3f
Jully <jullysitapara@gmail.com>
2024-05-28 14:31:24 +0530

    add hover effect to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,15 @@
-import React, {useState} from "react";
-import {Dialog} from '@headlessui/react'
-import {Button} from "@nextui-org/button";
-import {FaBars} from "react-icons/fa";
-import {FaXmark} from "react-icons/fa6";
-import {MdAddIcCall} from "react-icons/md";
+import React, { useState } from "react";
+import { Dialog } from '@headlessui/react'
+import { Button } from "@nextui-org/button";
+import { FaBars } from "react-icons/fa";
+import { FaXmark } from "react-icons/fa6";
+import { MdAddIcCall } from "react-icons/md";
+import { motion } from "framer-motion";
 
 const navigation = [
-    {name: 'About', href: '#about'},
-    {name: 'Products', href: '#products'},
-    {name: 'Contact', href: '#'},
+    { name: 'About', href: '#about' },
+    { name: 'Products', href: '#products' },
+    { name: 'Contact', href: '#' },
 ]
 
 const Header = () => {
@@ -19,7 +20,7 @@ const Header = () => {
             <nav className="mx-auto flex px-8 lg:px-64 items-center justify-between p-6" aria-label="Global">
                 <a href="#" className="">
                     <span className="sr-only">Laconic Group of Companies</span>
-                    <img className="h-10 lg:h-14 w-auto" src="/logo.png" alt=""/>
+                    <img className="h-10 lg:h-14 w-auto" src="/logo.png" alt="" />
                 </a>
                 <div className="flex lg:hidden">
                     <button
@@ -28,24 +29,38 @@ const Header = () => {
                         onClick={() => setMobileMenuOpen(true)}
                     >
                         <span className="sr-only">Open main menu</span>
-                        <FaBars className="h-6 w-6" aria-hidden="true"/>
+                        <FaBars className="h-6 w-6" aria-hidden="true" />
                     </button>
                 </div>
-                <div className="hidden lg:flex lg:items-center text-primary lg:gap-x-12">
+                <div className="hidden lg:flex lg:items-center lg:gap-x-12">
                     {navigation.map((item) => (
-                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6">
+                        <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-primary hover:text-opacity-60">
                             {item.name}
                         </a>
                     ))}
+                    {/* <span className="relative flex">
+                        <span className="animate-ping absolute inline-flex h-10 w-14 rounded-xl bg-primary opacity-75 "></span>
+                        // <span className="relative inline-flex rounded-xl h-10 w-14 bg-primary"></span>
+                        <span
+                            // href="tel:919429653388"
+                            className="relative inline-flex lg:flex lg:flex-row px-4 py-2 text-white rounded-xl bg-primary items-center overflow-hidden"
+                        >
+                            <a href="tel:919429653388">
+                                <MdAddIcCall />
+                                CALL US
+                            </a>
+                        </span>
+                    </span> */}
+
                     <Button as="a" href="tel:919429653388"
-                            className="hidden lg:flex lg:flex-row px-4 py-2 text-white rounded-xl bg-primary">
-                        <MdAddIcCall/>
+                        className="hidden lg:flex lg:flex-row px-4 py-2 text-white rounded-xl bg-primary hover:bg-opacity-75">
+                        <MdAddIcCall />
                         CALL US
                     </Button>
                 </div>
             </nav>
             <Dialog className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10"/>
+                <div className="fixed inset-0 z-10" />
                 <Dialog.Panel
                     className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
@@ -63,7 +78,7 @@ const Header = () => {
                             onClick={() => setMobileMenuOpen(false)}
                         >
                             <span className="sr-only">Close menu</span>
-                            <FaXmark className="h-6 w-6 text-primary" aria-hidden="true"/>
+                            <FaXmark className="h-6 w-6 text-primary" aria-hidden="true" />
                         </button>
                     </div>
                     <div className="mt-6 flow-root">
