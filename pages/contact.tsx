2117fb8ef4e552b987a9ae79997a88594ae64e75
Raj Suvariya <rajsuvariya@gmail.com>
2024-06-01 18:42:19 +0530

    add parallax to contact us page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,8 +2,10 @@
 const Contact = () => {
     return (
         <div className="flex flex-col justify-between">
-            <div className="">
-                <img alt={"contact us image"} src="/contactUs.jpg"/>
+            <div className="flex items-center justify-center bg-fixed h-96 bg-parallax bg-cover">
+                <h1 className="text-5xl text-primary">
+                    CONTACT US
+                </h1>
             </div>
         </div>
     )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -41,9 +41,7 @@ const config: Config = {
                 },
             },
             backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic":
-                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                "parallax" : 'url("../public/contactUs.jpg")',
             },
             colors: {
                 "primary": {
