04f97ece27a75ead6cb0ac082e4c835a157ddfbf
Jully <jullysitapara@gmail.com>
2024-06-25 18:25:47 +0530

    .

---
+++ components/SafeEnvironment.tsx
@@ -0,0 +1,61 @@
+import { RevelDivHorizontal } from "./revealDivHorizontal"
+import { RevelImgHorizontal } from "./revealImageHorizontal"
+
+const environment = [
+    {
+        subtitle: "100% BIODEGRADABLE & PLASTIC FREE",
+        title: "Made From Bagasse",
+        description: "Sugarcane Bagasse could be a stringy by-product of sugar industry. The husk which is cleared out after the juice has been extricated from the sugarcane. It’s natural properties make it a extraordinary crude fabric for making flatware items such as bowls, plates, takeaway holders. Bagasse is additionally a idealize substitution for plastics & paper in retail bundling.",
+        image: "/bagasse.jpg",
+    },
+    {
+        subtitle: "NATURAL RAW MATERIALS",
+        title: "Certified Food Safe",
+        description: "As an critical connect of the nourishment industry, we get it our ‘shared responsibility’ towards nourishment security and recognize the coordinate affiliation of nourishment serving & bundling holders with the quality and security of nourishment. All our items routinely experience thorough tests with universal compliance measures to guarantee they are nourishment secure.",
+        image: "/foodSafe.jpg",
+    },
+    {
+        subtitle: "NO PLASTIC IS FANTASTIC",
+        title: "Say No To Plastics",
+        description: "Plastic isn’t biodegradable and does not go absent; It’ll take hundreds of a long time for a single piece of plastic to break down, and after that it’ll turn into a perilous microplastic. Avoiding plastic could be a way better arrangement than reusing as the reusing handle too is carbon seriously.",
+        image: "/plastics.jpeg",
+    },
+]
+
+const SafeEnvironment = () => {
+
+    return (
+        <div className="flex flex-col gap-6 mb-24">
+            {environment.map((item, index) => {
+                return (
+                    <div
+                        className={`flex flex-col lg:h-full w-full ${index % 2 == 0 ? 'lg:flex-row' : 'lg:flex-row-reverse'}`}
+                        key={index}
+                    >
+                        <div className="w-4/5 lg:w-2/5 2xl:w-2/6 h-96 flex">
+                            <RevelImgHorizontal src={item.image} className="w-full object-fill" reverseDirection={index % 2 != 0} />
+                        </div>
+                        <div className="flex flex-col justify-center w-2/5 px-10 h-96 bg-primary2 text-white">
+                            <RevelDivHorizontal
+                                reverseDirection={index % 2 == 0}
+                                className="flex flex-col justify-center gap-8">
+                                <div className="flex flex-col max-lg:text-center gap-1 lg:gap-2">
+                                    <h1 className="font-thin text-sm" >{item.subtitle}</h1>
+                                    <h1 className="font-bold text-3xl">{item.title}</h1>
+                                </div>
+
+                                <p className="lg:text-base max-lg:text-center">
+                                    {item.description}
+                                </p>
+
+                            </RevelDivHorizontal>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+
+}
+
+export default SafeEnvironment
--- pages/bioware/bioware_home.tsx
+++ pages/bioware/bioware_home.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { FaDownload } from "react-icons/fa";
+import SafeEnvironment from "@/components/SafeEnvironment";
 
 const images = {
     carouselImages: [
@@ -34,103 +36,102 @@ const images = {
 
 const BiowareHome = () => {
     return (
-        <div className="flex flex-col gap-20 justify-center items-center">
-            <Carousel
-                className="w-full"
-                autoPlay={true}
-                infiniteLoop={true}
-                showStatus={false}
-                stopOnHover={false}
-                showArrows={false}
-                dynamicHeight={true}
-                interval={2000}
-                showThumbs={false}
-            >
-                {images.carouselImages.map((item, index) => {
-                    return (
-                        <div className="h-96 lg:h-screen" key={index}>
-                            <Image alt={item.alt} src={item.src} fill={true} />
-                        </div>
-                    )
-                })}
-            </Carousel >
-            <div className="flex flex-row relative">
-                <div className="absolute z-10 mr-12 translate-y-1/2 translate-x-10">
-                    <img src="/bioware_1.jpeg" alt="bioware image" className="w-[91%]" />
-                </div>
-                <div className="bg-primary2 py-12 pl-48 pr-10 flex flex-col text-white gap-7 w-[60%] translate-x-[53%]">
-                    <div>
-                        <h6 className="mb-1">ABOUT US</h6>
-                        <h1 className="text-3xl font-semibold">WELCOME TO LACONIC BIOWARE</h1>
+        <>
+            <div className="flex flex-col gap-20 justify-center items-center">
+                <Carousel
+                    className="w-full"
+                    autoPlay={true}
+                    infiniteLoop={true}
+                    showStatus={false}
+                    stopOnHover={false}
+                    showArrows={false}
+                    dynamicHeight={true}
+                    interval={2000}
+                    showThumbs={false}
+                >
+                    {images.carouselImages.map((item, index) => {
+                        return (
+                            <div className="h-96 lg:h-screen" key={index}>
+                                <Image alt={item.alt} src={item.src} fill={true} />
+                            </div>
+                        )
+                    })}
+                </Carousel >
+                <div className="flex flex-row relative">
+                    <div className="absolute z-10 mr-12 translate-y-1/2 translate-x-10">
+                        <img src="/bioware_1.jpeg" alt="bioware image" className="w-[91%]" />
                     </div>
-                    <p className="text-base font-light">
-                        Laconic Bioware is eco- friendly best baggase product manufacturer in India that specializes
-                        in producing high-quality, eco-friendly plates for both personal and commercial use. We make
-                        sustainable tableware products that are 100% eco-friendly and biodegradable. Our company is
-                        committed to using sustainable materials and processes in all aspects of our business, from
-                        sourcing our raw materials to packaging and shipping our products.
-                    </p>
-                    <p className="text-base font-light">
-                        Our team is made up of experienced professionals who are passionate about creating products that
-                        not only meet the needs of customers but also protect the environment. We take pride in the fact
-                        that our plates are biodegradable, meaning they can be safely broken down and returned to the
-                        earth without causing harm.
-                    </p>
-                    <button className="bg-white text-primary2 px-6 py-2 w-fit rounded-md mt-6">Read More</button>
+                    <div className="bg-primary2 py-12 pl-48 pr-10 flex flex-col text-white gap-7 w-[60%] translate-x-[53%]">
+                        <div>
+                            <h6 className="mb-1">ABOUT US</h6>
+                            <h1 className="text-3xl font-semibold">WELCOME TO LACONIC BIOWARE</h1>
+                        </div>
+                        <p className="text-base font-light">
+                            Laconic Bioware is eco- friendly best baggase product manufacturer in India that specializes
+                            in producing high-quality, eco-friendly plates for both personal and commercial use. We make
+                            sustainable tableware products that are 100% eco-friendly and biodegradable. Our company is
+                            committed to using sustainable materials and processes in all aspects of our business, from
+                            sourcing our raw materials to packaging and shipping our products.
+                        </p>
+                        <p className="text-base font-light">
+                            Our team is made up of experienced professionals who are passionate about creating products that
+                            not only meet the needs of customers but also protect the environment. We take pride in the fact
+                            that our plates are biodegradable, meaning they can be safely broken down and returned to the
+                            earth without causing harm.
+                        </p>
+                        <button className="bg-white text-primary2 px-6 py-2 w-fit rounded-md mt-6">Read More</button>
+                    </div>
                 </div>
-            </div>
-            <div className="my-24 relative flex flex-col">
-                <div className="grid grid-cols-4 gap-4 px-2">
-                    {images.gridImages.map((item, index) => {
-                        return (
-                            <div className={`z-10 ${index % 2 == 0 ? "translate-y-[15%]" : " "}`} key={index}>
-                                <img alt={item.alt} src={item.src} />
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="bg-primary2 text-center text-white pt-72 pb-44 gap-8 flex flex-col -my-28 ">
-                    <h6 className="text-4xl font-semibold">Our Product</h6>
-                    <p className="text-base font-light px-64">
-                        Laconic is more than just a tableware baggase products manufacturer. We are a team of individuals
-                        who are passionate about the environment and committed to finding sustainable solutions for
-                        everyday needs.
-                    </p>
-                </div>
-                <div className="bg-gray-100 border-solid border mx-40 px-3 py-12 grid grid-cols-6 items-center">
-                    {images.gridProductsBagasse.map((item, index) => {
-                        return (
-                            <div className="flex justify-center" key={index}>
-                                <img alt={item.alt} src={item.src} className="w-11/12 h-11/12" />
-                            </div>
-                        )
-                    })}
-                </div>
-                <div className="grid grid-cols-3 grid-rows-2 px-40 mt-20 gap-y-14 justify-items-center">
-                    {images.gridProducts.map((item, index) => {
-                        return (
-                            <div className="bg-primary2 w-80 h-36 rounded-tl-xl rounded-b-[70px] rounded-tr-[95px] flex flex-row items-center"
-                                key="index"
-                            >
-                                <div className="w-1/2 text-center">
-                                    <span className="text-white font-bold text-xl">{item.name}</span>
+                <div className="my-24 relative flex flex-col">
+                    <div className="grid grid-cols-4 gap-4 px-2">
+                        {images.gridImages.map((item, index) => {
+                            return (
+                                <div className={`z-10 ${index % 2 == 0 ? "translate-y-[15%]" : " "}`} key={index}>
+                                    <img alt={item.alt} src={item.src} />
                                 </div>
-                                <img src={item.src} alt={item.alt} className="w-32 h-32 self-start ml-10 -translate-y-5" />
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                    <div className="bg-primary2 text-center text-white pt-72 pb-44 gap-8 flex flex-col -my-28 ">
+                        <h6 className="text-4xl font-semibold">Our Product</h6>
+                        <p className="text-base font-light px-64">
+                            Laconic is more than just a tableware baggase products manufacturer. We are a team of individuals
+                            who are passionate about the environment and committed to finding sustainable solutions for
+                            everyday needs.
+                        </p>
+                    </div>
+                    <div className="bg-gray-100 border-solid border mx-40 px-3 py-12 grid grid-cols-6 items-center">
+                        {images.gridProductsBagasse.map((item, index) => {
+                            return (
+                                <div className="flex justify-center" key={index}>
+                                    <img alt={item.alt} src={item.src} className="w-11/12 h-11/12" />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="grid grid-cols-3 grid-rows-2 px-40 mt-20 gap-y-14 justify-items-center">
+                        {images.gridProducts.map((item, index) => {
+                            return (
+                                <div className="bg-primary2 w-80 h-36 rounded-tl-xl rounded-b-[70px] rounded-tr-[95px] flex flex-row items-center"
+                                    key="index"
+                                >
+                                    <div className="w-1/2 text-center">
+                                        <span className="text-white font-bold text-xl">{item.name}</span>
+                                    </div>
+                                    <img src={item.src} alt={item.alt} className="w-32 h-32 self-start ml-10 -translate-y-5" />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <button className="bg-primary2 text-white px-20 py-2 rounded-md mt-16 w-fit self-center">
+                        <a href="/Laconic Biowares Catalog - Laconic Group.pdf" download="Laconic Biowares Catalog - Laconic Group.pdf" target="_blank">
+                            <div className="flex items-baseline gap-2" ><FaDownload /> Download Catalog </div>
+                        </a>
+                    </button>
                 </div>
-                <button className="bg-primary2 text-white px-20 py-2 rounded-md mt-16 w-fit self-center">
-                    <a href="/Laconic Biowares Catalog - Laconic Group.pdf" download="Laconic Biowares Catalog - Laconic Group.pdf" target="_blank">
-                        Download Brochure
-                    </a>
-                </button>
             </div>
-            <div className="">
-                
-            </div>
-
-        </div>
+            <SafeEnvironment />
+        </>
     )
 }
 
